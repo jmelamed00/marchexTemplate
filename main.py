2a45e1b50673951c6a02ab885e17df97653badc6
Joel Melamed <63209350+jmelamed00@users.noreply.github.com>
2023-10-19 11:35:17 -0400

    Small clean up.

--- main.py
+++ main.py
@@ -8,22 +8,25 @@ header = {'Accept': 'text/plain', 'Content-Type': 'application/json', 'x-organiz
 # Collect all Marketing Edge Entities from APIs
 entities = useThreadsToCollectEntities(header)
 print('All Entities collected.')
-count = 0
 
-# Read data from a file
-data = pd.read_excel(r'C:\Users\jmelamed\PycharmProjects\template\notificationFails.xlsx', sheet_name='Sheet1')
+# Read data from a file that lives in the same directory as this code.
+data = pd.read_excel(r'updateGroups.xlsx', sheet_name='Sheet1')
 
 # Setup output storage
 goodData = []
 failData = []
+idICareAbout = 0
 for index, row in data.iterrows():
-    campaignID = row.CampaignId
-    campaignName = row.CampaignName
+    campaignName = row['Campaign Name']
 
-    if campaignID % 2 == 0:
-        goodData.append({'Campaign Name': str(row['Campaign Name']), 'Campaign ID': str(row['Campaign ID'])})
+    for group in entities['Groups']:
+        if group['name'] == campaignName:
+            idICareAbout = group['id']
+
+    if idICareAbout % 2 == 0:
+        goodData.append({'Campaign Name': str(row['Campaign Name'])})
     else:
-        failData.append({'Campaign Name': str(row['Campaign Name']), 'Campaign ID': str(row['Campaign ID'])})
+        failData.append({'Campaign Name': str(row['Campaign Name'])})
 
 pd.DataFrame(goodData).to_excel('GoodResults_start_' + start_time + '.xlsx', index=False)
 pd.DataFrame(failData).to_excel('FailResults_start_' + start_time + '.xlsx', index=False)
