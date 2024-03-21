4da88ae731a80ffb113c1670306823cf749e4760
Joel Melamed <63209350+jmelamed00@users.noreply.github.com>
2024-03-20 21:45:00 -0400

    This code only updates all groups it found to being channel with the appropriate rewrite rules.
    It has commented out the code that sets the call features.

--- main.py
+++ main.py
@@ -9,82 +9,86 @@ header = {'Accept': 'text/plain', 'Content-Type': 'application/json', 'x-organiz
 entities = useThreadsToCollectEntities(header)
 print('All Entities collected.')
 
-# Read data from a file that lives in the same directory as this code.
-inputFile = pd.read_excel(r'testingJM2.xlsx', sheet_name='Sheet1')
-
-# URLs to delete a number. Both require IDs appended.
-numberURL = 'https://edgeapi.marchex.io/marketingedge/v5/api/numbers/'
-poolURL = 'https://edgeapi.marchex.io/marketingedge/v5/api/numberpools/'
+# URL to access Groups
 groupURL = 'https://edgeapi.marchex.io/marketingedge/v5/api/Groups/'
+numbersURL = 'https://edgeapi.marchex.io/marketingedge/v5/api/numbers/'
 
+# Hard coded value grabbed from the UI for Kia Service - Aftersales
+# snippet_id = '4a692a3e-c6b0-4550-8f99-10d4e2dd2f8e'
+# This snippet is from Joel M Demo Org and restricted to Big O domain.
+# Nissan snippet id: cecd885958ed41a4b9cd1d10b9167838
+# Infinity snippet id: fecc5b5774f1440cacb18c68249339f0
+snippet_id = 'fecc5b5774f1440cacb18c68249339f0'
+# call_record_notify_file = '51a1de47-681d-4c4e-83f3-4bbf0ca45e6a'
 # Setup output storage
 goodData = []
 failData = []
 
 # Read in each row from the file.
-for index, row in inputFile.iterrows():
-    ctn = str(row['ctn'])
-    param_name = str(row['dni_type'])
-    dni_type = 'static'
-    if param_name[0].lower() == 'd':
-        dni_type = 'dni'
-
-    # Use the CTN from the file to find the data required to delete the CTN: ctnID for static and poolID for DNI
-    ctnID = -1
-    poolID = -1
-    if dni_type == 'static':
-        for number in entities['numbers']:
-            if ctn == number['phone_number']:
-                ctnID = number['id']
-                break
-    else:
-        apple = 0
-        for pool in entities['numberpools']:
-            apple += 1
-            banana = 0
-            if pool['status'].lower() == 'cancelled':
-                continue
-            for number in pool['numbers']:
-                banana += 1
-                if ctn == number['phone_number']:
-                    ctnID = number['id']
-                    poolID = pool['id']
-                    break
-            if ctnID > 0:
-                break
-
-    if ctnID == -1:
-        failData.append({'CTN': ctn, 'Failure Reason': 'Number does not exist.'})
-        continue
-
-    if dni_type == 'static':
-        response = requests.delete(numberURL + str(ctnID), headers=header)
-    else:
-        response = requests.delete(poolURL + str(poolID) + '/numbers/' + str(ctnID), headers=header)
-    if response.status_code != 200:
-        failData.append({'CTN': ctn, 'Failure Response': str(response.text)})
-    else:
-        goodData.append({'CTN': ctn})
-
-# This code will cancel every Group that doesn't have any active numbers.
 for group in entities['Groups']:
-    if group['dni_type'].lower() != 'session':
-        response = requests.get(groupURL + str(group['id']) + '/numbers', headers=header)
-    else:
-        response = requests.get(groupURL + str(group['id']) + '/numberpools', headers=header)
-    if response.status_code != 200:
-        failData.append({'Group ID': group['id'], 'Group Name': group['name'], 'Failed Get': str(response.text)})
+
+    if group['dni_type'] != 'None' or group['status'] != 'active':
         continue
 
-    responseText = json.loads(response.text)
-    count = 0
-    for number in responseText:
-        if number['status'].lower() == 'active':
-            count += 1
-    if count == 0:
-        response = requests.delete(groupURL + str(group['id']), headers=header)
-        if response.status_code != 200:
-            failData.append({'Group ID': group['id'], 'Group Name': group['name'], 'Failed Delete': str(response.text)})
+    # 1. Change group DNI type to Channel
+    payload = {'dni_type': 'OneToOne', 'global_snippet_id': snippet_id}
+    response = requests.put(groupURL + str(group['id']), json=payload, headers=header)
+    if response.status_code != 200:
+        failData.append({'Updating Group DNI': group['name'], 'Failure Reason': response.text})
+        continue
 
-pd.DataFrame(goodData).to_excel('GoodResults_start_' + start_time + '.xlsx', index=False)
-pd.DataFrame(failData).to_excel('FailResults_start_' + start_time + '.xlsx', index=False)
+    # 2. Grab all numbers associated with the group. There should be just the one when I do this for real.
+    response = requests.get(groupURL + str(group['id']) + '/numbers', headers=header)
+    if response.status_code != 200:
+        failData.append({'Getting Groups Numbers': group['name'], 'Failure Reason': response.text})
+        continue
+    numbers = json.loads(response.text)
+    if len(numbers) > 1:
+        failData.append({'Group': group['name'], 'Has too many numbers': len(numbers)})
+
+    # 3. Use each static number to create a channel number in the new group. Then delete the static number from the old group.
+    for number in numbers:
+        payload = {
+            'replacement_configuration': {
+                'numbers_to_replace': [number['call_routes']['route']['termination_number']],
+                'tracking_sources': [
+                        {
+                            'referrer_domain': 'Any',
+                            'url_triggers': [],
+                            'page_variable_triggers': [
+                                'page_var_source=epsilon',
+                                'page_var_medium=lp'
+                            ],
+                            'url_trigger_operator': 'OR',
+                            'page_trigger_operator': 'AND'
+                        }
+                ],
+                'replace_all_numbers': 'False'
+            }
+            # },
+            # 'call_routes': {
+            #     'route': {
+            #         'features': {
+            #             'call_record': {
+            #                 'enabled': 'True',
+            #                 'notification_file': call_record_notify_file,
+            #                 'record': 'True',
+            #                 'transcription_settings': {
+            #                     'transcript_enabled': 'True',
+            #                     'call_scoring_enabled': 'True'
+            #                 }
+            #             }
+            #         }
+            #     }
+            # }
+        }
+        response = requests.put(numbersURL + '/' + str(number['id']), headers=header, json=payload)
+        if response.status_code != 200:
+            failData.append({'Group Name': group['name'], 'Update Number Reason': response.text})
+            continue
+        goodData.append({'Updated Group': group['name'], 'Number Name': number['name']})
+
+if len(goodData):
+    pd.DataFrame(goodData).to_excel('GoodResults_start_' + start_time + '.xlsx', index=False)
+if len(failData):
+    pd.DataFrame(failData).to_excel('FailResults_start_' + start_time + '.xlsx', index=False)
